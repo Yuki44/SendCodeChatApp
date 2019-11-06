9c40667c7b879177699607cda382940fad5b372a
Yuki <carl7755@easv365.dk>
2019-11-07 00:14:57 +0100

    Activity for the chatroom list with fake chatrooms

---
+++ app/src/main/java/com/houseof/code/ChatroomsListActivity.java
@@ -0,0 +1,56 @@
+package com.houseof.code;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.houseof.code.adapters.ChatroomsRecyclerAdapter;
+import com.houseof.code.models.Chatroom;
+import com.houseof.code.util.VerticalSpacingItemDecorator;
+
+import java.util.ArrayList;
+
+public class ChatroomsListActivity extends AppCompatActivity {
+
+    private static final String TAG = "ChatroomsListActivity";
+
+    //UI Components
+    private RecyclerView mRecyclerViewWidget;
+
+    //Variables
+    private ArrayList<Chatroom> mChatrooms = new ArrayList<>();
+    private ChatroomsRecyclerAdapter mChatroomsRecyclerAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatrooms_list);
+        mRecyclerViewWidget = findViewById(R.id.recyclerViewChatrooms);
+
+        initRecyclerView();
+        insertFakeChatrooms();
+    }
+    
+    private void insertFakeChatrooms(){
+        for (int i = 0; i < 1000; i++) {
+            Chatroom chatroom = new Chatroom();
+            chatroom.setChatroomTitle("Chatroom title # " + i);
+            chatroom.setChatroomDescription("Example description # " + i);
+            chatroom.setChatroomPhoto("");
+            mChatrooms.add(chatroom);
+        }
+        mChatroomsRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerViewWidget.setLayoutManager(linearLayoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(5);
+        mRecyclerViewWidget.addItemDecoration(itemDecorator);
+        mChatroomsRecyclerAdapter = new ChatroomsRecyclerAdapter(mChatrooms);
+        mRecyclerViewWidget.setAdapter(mChatroomsRecyclerAdapter);
+    }
+}
