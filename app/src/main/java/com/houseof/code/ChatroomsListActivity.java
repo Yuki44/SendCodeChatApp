6ca597b8a4c01dd81bdcf4e3a1e76e878fe41f4d
Yuki <carl7755@easv365.dk>
2019-11-09 15:47:35 +0100

    chatroom list added get of chatrooms and check authentication

--- app/src/main/java/com/houseof/code/ChatroomsListActivity.java
+++ app/src/main/java/com/houseof/code/ChatroomsListActivity.java
@@ -1,20 +1,29 @@
 package com.houseof.code;
-
-import android.graphics.Bitmap;
+import android.content.Intent;
 import android.os.Bundle;
-import android.transition.Fade;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.houseof.code.adapters.ChatroomsRecyclerAdapter;
 import com.houseof.code.models.Chatroom;
 import com.houseof.code.util.VerticalSpacingItemDecorator;
@@ -33,54 +42,54 @@ public class ChatroomsListActivity extends AppCompatActivity implements Chatroom
     private RecyclerView mRecyclerViewWidget;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private CircleImageView mImageView;
+    private Toolbar mToolbar;
 
     //Variables
     private ArrayList<Chatroom> mChatrooms = new ArrayList<>();
     private ChatroomsRecyclerAdapter mChatroomsRecyclerAdapter;
+    private DocumentSnapshot mLastQueriedDocument;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatrooms_list);
         mRecyclerViewWidget = findViewById(R.id.recyclerViewChatrooms);
+
+        checkIfAuthenticated();
         findUiComponents();
         initRecyclerView();
-        insertFakeChatrooms();
         loadProfileImage();
+        loadChatrooms();
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-
+                loadChatrooms();
                 mChatroomsRecyclerAdapter.notifyDataSetChanged();
                 mSwipeRefreshLayout.setRefreshing(false);
             }
         });
-
     }
 
-    private void loadProfileImage() {
-        String imageUrl = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getPhotoUrl().toString();
-        Picasso.get().load(imageUrl)
-                .error(R.drawable.ic_user_no_photo)
-                .into(mImageView);
+    private void checkIfAuthenticated() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            Intent startIntent = new Intent(this, SplashScreenActivity.class);
+            startActivity(startIntent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
     }
 
     private void findUiComponents() {
         mRecyclerViewWidget = findViewById(R.id.recyclerViewChatrooms);
         mImageView = findViewById(R.id.profileImage);
         mSwipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        mToolbar = (Toolbar) findViewById(R.id.snippet_chatroomlist_toolbar);
+        setSupportActionBar(mToolbar);
     }
 
-    private void insertFakeChatrooms(){
-        for (int i = 0; i < 1000; i++) {
-            Chatroom chatroom = new Chatroom();
-            chatroom.setChatroomTitle("Chatroom title # " + i);
-            chatroom.setChatroomDescription("Example description # " + i);
-            chatroom.setChatroomPhoto("");
-            mChatrooms.add(chatroom);
-        }
-        mChatroomsRecyclerAdapter.notifyDataSetChanged();
-    }
 
     private void initRecyclerView(){
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -93,8 +102,81 @@ public class ChatroomsListActivity extends AppCompatActivity implements Chatroom
 
     @Override
     public void onChatroomClick(int position) {
-        //Intent intent = new Intent(this, NewActivity.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this, ChatroomActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         Log.d(TAG, "onChatroomClick: clicked");
     }
+
+    private void loadProfileImage() {
+        String imageUrl = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getPhotoUrl().toString();
+        Picasso.get().load(imageUrl)
+                .error(R.drawable.ic_user_no_photo)
+                .into(mImageView);
+    }
+
+    private void loadChatrooms() {
+        CollectionReference chatroomsCollectionRef = db
+                .collection("chatrooms");
+
+        Query chatroomsQuery = null;
+        if(mLastQueriedDocument != null){
+            chatroomsQuery = chatroomsCollectionRef.startAfter(mLastQueriedDocument);
+        }
+        else{
+            chatroomsQuery = chatroomsCollectionRef;
+        }
+
+        chatroomsQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+
+                    for(QueryDocumentSnapshot document: task.getResult()){
+                        Chatroom chatroom = document.toObject(Chatroom.class);
+                        mChatrooms.add(chatroom);
+                    }
+
+                    if(task.getResult().size() != 0){
+                        mLastQueriedDocument = task.getResult().getDocuments()
+                                .get(task.getResult().size() -1);
+                    }
+
+                    mChatroomsRecyclerAdapter.notifyDataSetChanged();
+                }
+                else{
+                    Log.e(TAG, "onComplete: Failed", task.getException());
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       super.onCreateOptionsMenu(menu);
+getMenuInflater().inflate(R.menu.menu_logout, menu);
+       return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+if(item.getItemId() == R.id.menuitem_logout){
+    FirebaseAuth.getInstance().signOut();
+    checkIfAuthenticated();
+}
+        return true;
+    }
+
+    /*  private void insertFakeChatrooms(){
+        for (int i = 0; i < 1000; i++) {
+            Chatroom chatroom = new Chatroom();
+            chatroom.setChatroomTitle("Chatroom title # " + i);
+            chatroom.setChatroomDescription("Example description # " + i);
+            chatroom.setChatroomPhoto("");
+            mChatrooms.add(chatroom);
+        }
+        mChatroomsRecyclerAdapter.notifyDataSetChanged();
+    }*/
+
 }
