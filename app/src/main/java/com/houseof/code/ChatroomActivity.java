b47f72fb544b29cebd0ac514c5274e597dcb00a0
Yuki <carl7755@easv365.dk>
2019-11-09 01:38:44 +0100

    added chatroom activity

---
+++ app/src/main/java/com/houseof/code/ChatroomActivity.java
@@ -0,0 +1,21 @@
+package com.houseof.code;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatroomActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+}
