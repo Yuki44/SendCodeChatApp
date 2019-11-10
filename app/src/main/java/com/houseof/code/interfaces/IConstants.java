e305f60280cf8c7cc58ef3b769c13dccbd9ee67b
Yuki <carl7755@easv365.dk>
2019-11-11 00:51:53 +0100

    added some constants to order, limit and query ref

---
+++ app/src/main/java/com/houseof/code/interfaces/IConstants.java
@@ -0,0 +1,8 @@
+package com.houseof.code.interfaces;
+
+
+public interface IConstants {
+    String MESSAGES_COLLECTION = "messages";
+    String MESSAGE_TIMESTAMP_PROPERTY = "messageTimestamp";
+    int LIMIT = 50;
+}
