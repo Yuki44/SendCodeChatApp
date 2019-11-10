b5f3ba0e4f154699886c959ee54a5a1faff91d63
Yuki <carl7755@easv365.dk>
2019-11-11 00:51:22 +0100

    added model to handle firestore operations depending on the type

---
+++ app/src/main/java/com/houseof/code/models/Operation.java
@@ -0,0 +1,13 @@
+package com.houseof.code.models;
+
+public class Operation {
+    public Message message;
+    public int type;
+
+    public Operation(Message message, int type) {
+        this.message = message;
+        this.type = type;
+    }
+
+
+}
